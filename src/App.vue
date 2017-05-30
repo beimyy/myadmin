63a188d0dec2af911906d60687a5c82baf88b9fa
beimyy <beimyy@163.com>
2017-05-31 00:05:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3">
+          <a href="#" target="_blank">订单管理</a>
+        </el-menu-item>
+      </el-menu>
+    </el-row>
+
+    <div class="line"></div>
+    
+    <el-row class="tac">
+      <el-col :span="3">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-message"></i>导航一
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
+          <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+          
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-setting"></i>导航四  
+            </template>
+            <el-menu-item index="4-1">选项41</el-menu-item>
+            <el-menu-item index="4-2">选项42</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="1000">
+            <template slot="title">
+                <i class="el-icon-setting"></i>基础组件
+            </template>
+            <el-menu-item index="1001">
+              <router-link :to="{path: '#'}">Layout 布局</router-link>
+            </el-menu-item>
+            <el-menu-item index="1002">
+              <router-link :to="{path: '#'}">Layout 布局</router-link>
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </el-col>
+    </el-row>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+</style>
